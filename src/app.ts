dae66c7ced307b51e1997318c45c5199589e8e05
Jafir Anjum <jafiranjum2020@gmail.com>
2024-06-12 17:27:20 +0600

    not found middleware added

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express, { Request, Response } from 'express';
+import notFound from './middlwares/notFound';
+
 
 const app = express();  
 app.get('/', (req:Request, res: Response) => {
  res.send('Hello World!')
 
 })
+
+//Not FOUND
+app.use(notFound)
 
 export default app;
